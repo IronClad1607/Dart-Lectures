7e5a00041a723e26ef47f2aab8ba82977163407b
Ishaan Jain <ishaan160799@gmail.com>
2020-04-27 20:57:59 +0530

    minor changes

--- Classes.dart
+++ Classes.dart
@@ -1,19 +1,15 @@
 class Person {
-  Person({this.name, this.age, this.height });
+  Person({this.name, this.age, this.height});
 
   final String name;
   final int age;
-  final  double height;
+  final double height;
 
   String describeArrow() =>
       "Hello, I'm $name. I'm $age years old,I'm $height meters tall.";
 }
 
 void main() {
-  final person = Person(name: 'Ishaan',age: 20,height: 1.74);
+  final person = Person(name: 'Ishaan', age: 20, height: 1.74);
   print(person.describeArrow());
 }
-
-
-
-
