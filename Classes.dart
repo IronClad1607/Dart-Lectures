21e2841f58bfb6ff62e384899f61b824bd741a3d
Ishaan Jain <ishaan160799@gmail.com>
2020-04-27 20:52:45 +0530

    Classes 3

--- Classes.dart
+++ Classes.dart
@@ -4,16 +4,16 @@ class Person {
   final String name;
   final int age;
   final  double height;
+
+  String describeArrow() =>
+      "Hello, I'm $name. I'm $age years old,I'm $height meters tall.";
 }
 
 void main() {
   final person = Person(name: 'Ishaan',age: 20,height: 1.74);
-  print(person.name);
+  print(person.describeArrow());
 }
 
-String describe({var name, var age, var height}) {
-  return "Hello, I'm $name. I'm $age years old,I'm $height meters tall.";
-}
 
-String describeArrow({var name, var age, var height}) =>
-    "Hello, I'm $name. I'm $age years old,I'm $height meters tall.";
+
+
