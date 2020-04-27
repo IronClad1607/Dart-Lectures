b7f4013c22fd3adbd79f6017e7da2e9639324caf
Ishaan Jain <ishaan160799@gmail.com>
2020-04-27 20:47:56 +0530

    Classes 2

--- Classes.dart
+++ Classes.dart
@@ -1,11 +1,14 @@
 class Person {
-  String name;
-  int age;
-  double height;
+  Person({this.name, this.age, this.height });
+
+  final String name;
+  final int age;
+  final  double height;
 }
 
 void main() {
-  final person = Person();
+  final person = Person(name: 'Ishaan',age: 20,height: 1.74);
+  print(person.name);
 }
 
 String describe({var name, var age, var height}) {
