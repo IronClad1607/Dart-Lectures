3eadfe686cb511e30eca5ce3b69e42a6534e1462
Ishaan Jain <ishaan160799@gmail.com>
2020-04-27 21:02:36 +0530

    Classes 3

--- Classes.dart
+++ Classes.dart
@@ -1,19 +1,27 @@
 class Person {
-  Person({this.name, this.age, this.height });
+  Person({this.name, this.age, this.height});
 
   final String name;
   final int age;
-  final  double height;
+  final double height;
 
   String describeArrow() =>
       "Hello, I'm $name. I'm $age years old,I'm $height meters tall.";
+
+  void sayName() => print("I'm $name");
+}
+
+class Employee extends Person {
+  final String taxCode;
+  final int salary;
+
+  Employee({this.taxCode, this.salary});
 }
 
 void main() {
-  final person = Person(name: 'Ishaan',age: 20,height: 1.74);
+  final person = Person(name: 'Ishaan', age: 20, height: 1.74);
   print(person.describeArrow());
+  final employee = Employee(taxCode: 'AB12', salary: 50000);
+
+  employee.sayName();
 }
-
-
-
-
